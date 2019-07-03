346f913c9552471296d1516aab981e434fa58d83
ANTO RAGHAVAN <antragha@in.ibm.com>
2019-07-03 11:20:25 +0530

    forecat

--- src/utils/forecast.js
+++ src/utils/forecast.js
@@ -3,18 +3,14 @@ const request = require('request')
 const forecast = (latitude, longitude, callback) => {
 
     const url = 'https://api.darksky.net/forecast/79f071eb75b8832c99a15e6d56bbb512/'+ latitude + ',' + longitude
-    request({ url: url, json: true }, (error, response) => {
+    request({ url, json: true }, (error, { body }) => {
         if (error) {
             callback('Unable to connect to weather service!', undefined)
-        } else if (response.body.error) {
+        } else if (body.error) {
             callback('Unable to find location', undefined)
         } else {
-            callback(undefined, body.daily.data[0].summary + 
-                ' It is currently ' + body.currently.temperature + 
-                ' degress out. This high today is ' + body.daily.data[0].temperatureHigh + 
-                ' with a low of ' + body.daily.data[0].temperatureLow + 
-                '. There is a ' + body.currently.precipProbability + '% chance of rain.')
-             }
+            callback(undefined, body.daily.data[0].summary + ' It is currently ' + body.currently.temperature + ' degress out. This high today is ' + body.daily.data[0].temperatureHigh + ' with a low of ' + body.daily.data[0].temperatureLow + '. There is a ' + body.currently.precipProbability + '% chance of rain.')
+        }
     })
 }
 
